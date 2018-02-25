e933738700efb8616f07a3267d8dc2c71ee694e8
James McDaniel <icathaid@gmail.com>
2018-02-24 16:54:47 -0800

    tidied up code

--- js/app.js
+++ js/app.js
@@ -1,10 +1,4 @@
 'use strict';
-function pol(){
-    alert('proof of life');
-}
-//-=-=-=-=-=-=-=-test
-//pullData();
-
 //-=-=-=-=-=-=-=-vars
 var allProducts = [];
 var prodBox = [];
@@ -22,7 +16,7 @@ var data = {
             backgroundColor: ['navy', 'red', 'navy', 'red', 'navy', 'red', 'navy', 'red', 'navy', 'red', 'navy', 'red', 'navy', 'red', 'navy', 'red', 'navy', 'red', 'navy', 'red', 'navy', 'red', 'navy', 'red'],
         }]
     };
-//-=-=-=-=-=-=-=-constructor functions
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-fun
 function Product(name, filepath, title, clicks, views, percentage) {
     this.name = name;
     this.filepath = filepath;
@@ -31,12 +25,7 @@ function Product(name, filepath, title, clicks, views, percentage) {
     this.views = views;
     allProducts.push(this);
 };
-
-//-=-=-=-=-this kind of works, leaving it here as a template for other functions
-//build out other kinds of graphs until you figure out what part of the syntax you're fucking up
-
-
-//-=-=-=-=-=-WRAP THIS IN A FOR LOOP-=-=-=-=-=-=-
+//-=-=-=-=-=-Create New Products-=-=-=-=-=-=-
 new Product('bag', 'img/bag.jpg', 'R2D2 Luggage', 0, 0);
 new Product('banana', 'img/banana.jpg', 'Banana Slicer', 0, 0);
 new Product('bathroom', 'img/bathroom.jpg', 'Toilet Paper Tablet Holder', 0, 0);
@@ -57,17 +46,16 @@ new Product('unicorn', 'img/unicorn.jpg', 'True story - I have owned this actual
 new Product('usb', 'img/usb.gif', 'Monster Tail USB Drive', 0, 0);
 new Product('water-can', 'img/water-can.jpg', 'Self-Watering Watering Can', 0, 0);
 new Product('wine-glass', 'img/wine-glass.jpg', 'Pythagorean Wine Glass', 0, 0);
-checkLocalStorage();
-//event listeners
 images.addEventListener('click', letsGo);
 prodPicA.addEventListener('click', tallyVotesA);
 prodPicB.addEventListener('click', tallyVotesB);
 prodPicC.addEventListener('click', tallyVotesC);
-
+checkLocalStorage();
 //this runs on page load to fill up prodBox with 6 unique numbers
 for (var i = 0; i < 6; i++){
     makeRandomProd();
 };
+//Makes a new random product
 function makeRandomProd(){
     var randomProd = Math.floor(Math.random() * allProducts.length);
     if (prodBox.includes(randomProd)){
@@ -76,7 +64,6 @@ function makeRandomProd(){
         prodBox.push(randomProd);
         if (prodBox.length > 6){
             prodBox.shift();
-            //console.log(prodBox);
         };
     };
 };
@@ -85,7 +72,6 @@ function newProds(){
     for (var j = 0; j < 3; j++){
         makeRandomProd();
     }
-    //console.log(prodBox);
 };
 //-=-=-=-=-draw the first three images and log that they were viewed:
 prodPicA.src = allProducts[prodBox[0]].filepath;
@@ -97,7 +83,6 @@ allProducts[prodBox[1]].views +=1;
 prodPicC.src = allProducts[prodBox[2]].filepath;
 prodPicC.title = allProducts[prodBox[2]].title;
 allProducts[prodBox[2]].views +=1;
-
 //-=-=-=-=-draw three new images and log that each was viewed:
 function letsGo(){
     if (clickCount < 25){
@@ -113,28 +98,20 @@ function letsGo(){
         allProducts[prodBox[2]].views +=1;
         clickCount +=1;
     } else {
-        //console.table(allProducts);
         images.removeEventListener('click', letsGo);
         prodPicA.removeEventListener('click', tallyVotesA);
         prodPicB.removeEventListener('click', tallyVotesB);
         prodPicC.removeEventListener('click', tallyVotesC);
         countAllVotes();
-        //alert(clickCount + ' clicks!');
         calcPercentage();
-        console.table(allProducts);
         localStorage.setItem('allProducts', JSON.stringify(allProducts));
         return location.href = 'page2.html';
     };
 };
     
-//-=-=-=-=-=-=-Perhaps my only well-named function, this tallies votes.
 function tallyVotesA(){allProducts[prodBox[0]].clicks +=1;};
 function tallyVotesB(){allProducts[prodBox[1]].clicks +=1;};
 function tallyVotesC(){allProducts[prodBox[2]].clicks +=1;};
-//add function to each of these lines ^^^ to update local storage
-//then add function to check if local storage exists and/or set it up
-//figure out how to store an array of objects instead of coding this whole god damn thing
-
 function checkLocalStorage(){
     if (!localStorage.allProducts){
         localStorage.setItem('allProducts', JSON.stringify(allProducts));
@@ -142,18 +119,10 @@ function checkLocalStorage(){
         allProducts = JSON.parse(localStorage.getItem('allProducts'));
     };
 };
-
-//-=-=-=-=-=-=-fills arrays for chart
 function countAllVotes(){
     for (var i = 0; i < allProducts.length; i++){
         votes[i] = allProducts[i].clicks;
         crap[i] = allProducts[i].name;
-    }
-};
-//-=-=-=-=-test function
-function showVotes(){
-    for (var i = 0; i < allProducts.length; i++){
-        console.log('Product: ' + crap[i] + ' Votes: ' + votes[i]);
     }
 };
 function calcPercentage(){
@@ -161,21 +130,3 @@ function calcPercentage(){
         allProducts[i].percentage = allProducts[i].clicks / allProducts[i].views;
     };
 };
-//-=-=-=-=-=----test function
-function doshit(){
-    if (localStorage.getItem("bill")){
-        console.log('bill exists');
-        var ted = parseInt(localStorage.getItem("bill"));
-        console.log('bill: ' + localStorage.getItem("bill"));
-        console.log(ted);
-        ted +=1;
-        localStorage.setItem("bill", ted);
-    } else {
-        localStorage.setItem("bill", "0");
-    }
-};
-//-=-=-=-=-=-=-=-test function
-
-//if local storage exists else loop through allproducts and newproduct
-
-//product.all = json.parse(localstorage.busmall)
